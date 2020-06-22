613e176413c95f8c9b305d7f32537dc7013005c1
Amanda Hogan <anhogan@MacBook-Pro.attlocal.net>
2020-06-22 09:37:46 -0400

    Style rules modal

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { ModalDiv } from './Styles';
+
 class Rules extends Component {
   closeModal = (e) => {
     this.props.closeModal && this.props.closeModal(e)
@@ -12,18 +14,22 @@ class Rules extends Component {
     };
 
     return (
-      <div class="rulesModal">
-        <h3>Game of Life Rules</h3>
-        <p>The universe of the Game of Life consists of a two-dimensional grid containing square cells with one of two possible states: live or dead. Each cell has eight neighbors on the left, right, up, down, and diagonals. During the animation, cell transitions occur based on the following rules:</p>
-        <ul>
-          <li>Any cell with two or three live neighbors survives</li>
-          <li>Any dead cell with three live neighbors becomes a live cell</li>
-          <li>Any live cell with less than two live neighbors, or more than three live neighbors, dies</li>
-        </ul>
-        <p>To play, create an initial pattern on the grid, then click Start to watch the life cycle evolve. Alternatively, use the Random button to generate a random starting configuration of cells, or choose from a predefined configuration.</p>
-        <p>Use the speed boxes to determine how fast, or slow, the animation will play. To see a configuration at a certain generation, hit the Stop button. Use the Clear button to restart.</p>
-        <button onClick={this.closeModal}>Play</button>
-      </div>
+      <ModalDiv>
+        <h2>Game of Life Rules</h2>
+        <div className="content">
+          <p>The universe of the Game of Life consists of a two-dimensional grid containing square cells with one of two possible states: live or dead. Each cell has eight neighbors on the left, right, up, down, and diagonals. During the animation, cell transitions occur based on the following rules:</p>
+          <ul>
+            <li>Any cell with two or three live neighbors survives</li>
+            <li>Any dead cell with three live neighbors becomes a live cell</li>
+            <li>Any live cell with less than two live neighbors, or more than three live neighbors, dies</li>
+          </ul>
+          <p>To play, create an initial pattern on the grid, then click Start to watch the life cycle evolve. Alternatively, use the Random button to generate a random starting configuration of cells, or choose from a predefined configuration.</p>
+          <p>Use the speed boxes to determine how fast, or slow, the animation will play. To see a configuration at a certain generation, hit the Stop button. Use the Clear button to restart.</p>
+        </div>
+        <div className="actions">
+          <button className="playBtn" onClick={this.closeModal}>Play</button>
+        </div>
+      </ModalDiv>
     );
   }
 }
